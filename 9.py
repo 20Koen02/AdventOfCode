9cc369ee9abbf7e067a63f1c9d84fb2993102b69
20Koen02 <kvanwijn@online.nl>
2019-12-09 06:43:54 +0100

    8 finished, 9 start

--- 9.py
+++ 9.py
@@ -1,65 +1,65 @@
-def sum(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def sum(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
 
     out = accumulator[0] + accumulator[1]
     stack[stack[instructionPointer + 3]] = out
 
     instructionPointer += 4
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def mul(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def mul(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
 
     out = accumulator[0] * accumulator[1]
     stack[stack[instructionPointer + 3]] = out
 
     instructionPointer += 4
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def inp(stack, instructionPointer, parameters):
+def inp(stack, instructionPointer, parameters, relBase):
     out = int(input("Please provide a single integer: "))
     stack[stack[instructionPointer + 1]] = out
 
     instructionPointer += 2
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def out(stack, instructionPointer, parameters):
+def out(stack, instructionPointer, parameters, relBase):
     if parameters[0] == 1:
         print(stack[instructionPointer + 1])
     else:
         print(stack[stack[instructionPointer + 1]])
 
     instructionPointer += 2
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def jit(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def jit(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
     
     if accumulator[0] != 0:
         instructionPointer = accumulator[1]
     else:
         instructionPointer += 3
 
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def jif(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def jif(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
     
     if accumulator[0] == 0:
         instructionPointer = accumulator[1]
     else:
         instructionPointer += 3
 
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def lss(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def lss(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
     
     if accumulator[0] < accumulator[1]:
         stack[stack[instructionPointer + 3]] = 1
@@ -67,11 +67,11 @@ def lss(stack, instructionPointer, parameters):
         stack[stack[instructionPointer + 3]] = 0
 
     instructionPointer += 4
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
 
 
-def eql(stack, instructionPointer, parameters):
-    accumulator = getAccumulator(stack, instructionPointer, parameters)
+def eql(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
     
     if accumulator[0] == accumulator[1]:
         stack[stack[instructionPointer + 3]] = 1
@@ -79,11 +79,18 @@ def eql(stack, instructionPointer, parameters):
         stack[stack[instructionPointer + 3]] = 0
 
     instructionPointer += 4
-    return stack, instructionPointer
+    return stack, instructionPointer, relBase
+
+def rel(stack, instructionPointer, parameters, relBase):
+    accumulator = getAccumulator(stack, instructionPointer, parameters, relBase)
+    relBase = relBase + accumulator[0]
+
+    instructionPointer += 2
+    return stack, instructionPointer, relBase
 
 
 def main():
-    stack = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+    stack = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
     stack = list(map(int, stack.split(",")))
     instructionPointer = 0
 
@@ -95,8 +102,11 @@ def main():
         5: jit,
         6: jif,
         7: lss,
-        8: eql
+        8: eql,
+        9: rel
     }
+
+    relBase = 0
 
     while True:
         if stack[instructionPointer] == 99:
@@ -112,18 +122,21 @@ def main():
         parameters.reverse()
         parameters = parameters + [0]*(2 - len(parameters))
 
-        stack, instructionPointer = opcodes.get(op)(stack, instructionPointer, parameters)
+        stack, instructionPointer, relBase = opcodes.get(op)(stack, instructionPointer, parameters, relBase)
 
 
-def getAccumulator(stack, instructionPointer, parameters):
+def getAccumulator(stack, instructionPointer, parameters, relBase):
     accumulator = []
     for x in range(len(parameters)):
         if parameters[x] == 0:
             accumulator.append(stack[stack[instructionPointer + x + 1]])
         elif parameters[x] == 1:
             accumulator.append(stack[instructionPointer + x + 1])
+        elif parameters[x] == 2:
+            accumulator.append(stack[stack[instructionPointer + x + 1] + relBase])
     return accumulator
 
 
 if __name__ == "__main__":
     main()
+
