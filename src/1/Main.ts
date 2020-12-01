3372710996f0930fdb98a7adfd6a7804e83bbd7a
20Koen02 <mail@koen02.nl>
2020-12-01 16:01:48 +0100

    less loops - more better

--- src/1/Main.ts
+++ src/1/Main.ts
@@ -8,14 +8,11 @@ export const challenge = new Challenge('1', class Solver extends SolverBase {
 
   partOne () {
     let answer: string = '';
-    dance:
     for (let i = 0; i < this.exp.length; i++) {
-      for (let j = 0; j < this.exp.length; j++) {
-        if (i === j) continue;
-        if (this.exp[i] + this.exp[j] === 2020) {
-          answer = (this.exp[i] * this.exp[j]).toString();
-          break dance;
-        }
+      const num2: number = 2020 - this.exp[i];
+      if (this.exp.includes(num2)) {
+        answer = (this.exp[i] * num2).toString();
+        break;
       }
     }
     return answer;
@@ -23,16 +20,12 @@ export const challenge = new Challenge('1', class Solver extends SolverBase {
 
   partTwo () {
     let answer: string = '';
-    dance:
     for (let i = 0; i < this.exp.length; i++) {
+      const num2: number = 2020 - this.exp[i];
       for (let j = 0; j < this.exp.length; j++) {
-        if (i === j) continue;
-        for (let k = 0; k < this.exp.length; k++) {
-          if (j === k || i === k) continue;
-          if (this.exp[i] + this.exp[j] + this.exp[k] === 2020) {
-            answer = (this.exp[i] * this.exp[j] * this.exp[k]).toString();
-            break dance;
-          }
+        const num3: number = num2 - this.exp[j];
+        if (this.exp.includes(num3)) {
+          answer = (this.exp[i] * this.exp[j] * num3).toString();
         }
       }
     }
