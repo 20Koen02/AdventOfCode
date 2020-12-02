de75c60eda065b0ac81461efa03dd9981b50e8e2
20Koen02 <mail@koen02.nl>
2020-12-02 10:33:00 +0100

    Day 2

---
+++ src/2/Main.test.ts
@@ -0,0 +1,11 @@
+import { challenge } from './Main';
+
+describe('template', () => {
+  it('part 1 should equal to 398', async () => {
+    expect(challenge.partOne).toEqual('398');
+  });
+
+  it('part 2 should equal to 562', async () => {
+    expect(challenge.partTwo).toEqual('562');
+  });
+});
