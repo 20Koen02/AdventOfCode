d69641f89aad50a13fc35dc6f88a82c6ed2f3052
20Koen02 <mail@koen02.nl>
2020-12-05 12:22:16 +0100

    Day 5

---
+++ src/5/Main.ts
@@ -0,0 +1,23 @@
+import { Challenge } from '../Challenge';
+import { SolverBase } from '../SolverBase';
+
+export const challenge = new Challenge('5', class Solver extends SolverBase {
+  tickets: string[] = this.input.split('\n');
+
+  seats: number[] = this.tickets.map((ticket): number => {
+    const bin: string = ticket.replace(/[BR]/g, '1').replace(/[FL]/g, '0');
+    return parseInt(bin, 2);
+  })
+
+  partOne () {
+    return Math.max(...this.seats).toString();
+  }
+
+  partTwo () {
+    let res: number = 0;
+    this.seats.forEach((seat) => {
+      if (this.seats.includes(seat + 2) && !this.seats.includes(seat + 1)) res = seat + 1;
+    });
+    return res.toString();
+  }
+});
--- src/template/Main.ts
+++ src/template/Main.ts
@@ -2,11 +2,11 @@ import { Challenge } from '../Challenge';
 import { SolverBase } from '../SolverBase';
 
 export const challenge = new Challenge('template', class Solver extends SolverBase {
-  // partOne() {
+  // partOne () {
   //   return this.input;
   // }
 
-  // partTwo() {
+  // partTwo () {
   //   return this.input;
   // }
 });
