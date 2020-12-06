7def00dd0405910fb0cd1561291c710924738aad
20Koen02 <mail@koen02.nl>
2020-12-06 12:06:57 +0100

    Day 6

---
+++ src/6/Main.test.ts
@@ -0,0 +1,11 @@
+import { challenge } from './Main';
+
+describe('6', () => {
+  it('part 1 should equal to 6775', async () => {
+    expect(challenge.partOne).toEqual('6775');
+  });
+
+  it('part 2 should equal to 3356', async () => {
+    expect(challenge.partTwo).toEqual('3356');
+  });
+});
