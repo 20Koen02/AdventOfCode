dde60d26d3a1aecf011f1b121b5e27b070727a31
Koen van Wijngaarden <mail@koen02.nl>
2021-12-10 13:13:13 +0100

    further optimization for day 10

--- 10-rust/src/main.rs
+++ 10-rust/src/main.rs
@@ -5,25 +5,26 @@ const CLOSE_TAGS: &str = ")]}>";
 fn main() {
     let blocks: Vec<String> = INPUT.trim().split("\n").map(String::from).collect();
     let mut day_one = 0;
-    let mut day_two_scores: Vec<usize> = Vec::new();
-
-    '_blocks: for block in blocks {
-        let mut stack = Vec::new();
-        for c in block.chars() {
-            if let Some(i) = CLOSE_TAGS.chars().position(|p| c == p) {
-                if stack.pop() != OPEN_TAGS.chars().nth(i) {
-                    day_one += [3, 57, 1197, 25137][i];
-                    continue '_blocks;
+    let mut day_two_scores: Vec<usize> = blocks
+        .iter()
+        .filter_map(|block| {
+            let mut stack = Vec::new();
+            for c in block.chars() {
+                if let Some(i) = CLOSE_TAGS.chars().position(|p| c == p) {
+                    if stack.pop() != OPEN_TAGS.chars().nth(i) {
+                        day_one += [3, 57, 1197, 25137][i];
+                        return None;
+                    }
+                } else {
+                    stack.push(c);
                 }
-            } else {
-                stack.push(c);
             }
-        }
-        let day_two_score = stack.iter().rev().fold(0, |acc, &c| {
-            acc * 5 + OPEN_TAGS.chars().position(|p| c == p).unwrap() + 1
-        });
-        day_two_scores.push(day_two_score);
-    }
+            return Some(stack.iter().rev().fold(0, |acc, &c| {
+                acc * 5 + OPEN_TAGS.chars().position(|p| c == p).unwrap() + 1
+            }));
+        })
+        .collect::<Vec<usize>>();
+
     day_two_scores.sort();
     let day_two = day_two_scores[day_two_scores.len() / 2];
     println!("Day 10 part one: {}", day_one);
