c5165d611f4e2f1416bb564c9a366f15940438cc
Koen van Wijngaarden <mail@koen02.nl>
2022-12-01 11:16:17 +0100

    Day 1 2022

---
+++ 2022/day-01/src/main.rs
@@ -0,0 +1,23 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("in.txt");
+
+/// Vec of calories sorted from highest to lowest
+fn sorted_elves() -> Vec<usize> {
+    INPUT
+        .trim()
+        .split("\n\n")
+        .map(|elve| elve.lines().map(|c| c.parse::<usize>().unwrap()).sum())
+        .sorted()
+        .rev()
+        .collect::<Vec<usize>>()
+}
+
+fn main() {
+    let cal_totals = sorted_elves();
+    println!("Day 1 part one: {}", cal_totals[0]);
+    println!(
+        "Day 1 part two: {}",
+        cal_totals.iter().take(3).sum::<usize>()
+    );
+}
