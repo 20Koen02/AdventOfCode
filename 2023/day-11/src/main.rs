ea9f53953ce74d6290051bc4e3d7d276931e79d8
Koen van Wijngaarden <mail@koen02.nl>
2023-12-11 12:59:04 +0100

    refactor day 11

--- 2023/day-11/src/main.rs
+++ 2023/day-11/src/main.rs
@@ -1,43 +1,22 @@
-use std::ops::Sub;
-
 use helper::solved;
 use itertools::Itertools;
 
 const INPUT: &str = include_str!("in.txt");
 
-#[derive(Clone, Copy)]
-struct Point {
-    x: isize,
-    y: isize,
-}
+type Point = (usize, usize);
 
-impl Sub for Point {
-    type Output = usize;
-
-    fn sub(self, other: Self) -> Self::Output {
-        ((self.x - other.x).abs() + (self.y - other.y).abs()) as usize
-    }
-}
-
-/// Get the coordinates of all the galaxies
 fn get_coords(input: &str) -> Vec<Point> {
     input
         .lines()
         .enumerate()
         .fold(vec![], |mut acc, (y, line)| {
             line.chars().enumerate().for_each(|(x, c)| {
-                if c == '#' {
-                    acc.push(Point {
-                        x: x as isize,
-                        y: y as isize,
-                    });
-                }
+                if c == '#' { acc.push((x, y)); }
             });
             acc
         })
 }
 
-/// Get the indexes of the empty rows/columns
 fn get_empty_idx(idx: Vec<bool>) -> Vec<usize> {
     idx.iter()
         .enumerate()
@@ -45,54 +24,44 @@ fn get_empty_idx(idx: Vec<bool>) -> Vec<usize> {
         .collect_vec()
 }
 
-/// Expand the empty rows/columns
-fn expand_coords(mut coords: Vec<Point>, part_two: bool) -> Vec<Point> {
-    let max_y = coords.iter().max_by_key(|p| p.y).unwrap().y;
-    let max_x = coords.iter().max_by_key(|p| p.x).unwrap().x;
-    let mut rows = vec![true; max_y as usize + 1];
-    let mut cols = vec![true; max_x as usize + 1];
+fn expand_coords(coords: Vec<Point>, multiplier: usize) -> Vec<Point> {
+    let max_y = coords.iter().max_by_key(|p| p.1).unwrap().1;
+    let max_x = coords.iter().max_by_key(|p| p.0).unwrap().0;
+    let mut rows = vec![true; max_y + 1];
+    let mut cols = vec![true; max_x + 1];
 
     coords.iter().for_each(|p| {
-        rows[p.y as usize] = false;
-        cols[p.x as usize] = false;
+        rows[p.1] = false;
+        cols[p.0] = false;
     });
 
     let expand_row_idx = get_empty_idx(rows);
     let expand_col_idx = get_empty_idx(cols);
 
-    coords.iter_mut().for_each(|p| {
-        let move_x = expand_col_idx.iter().filter(|i| **i < p.x as usize).count() as isize;
-        let move_y = expand_row_idx.iter().filter(|i| **i < p.y as usize).count() as isize;
-
-        p.y += if part_two {
-            move_y * 1_000_000 - move_y
-        } else {
-            move_y
-        };
-        p.x += if part_two {
-            move_x * 1_000_000 - move_x
-        } else {
-            move_x
-        };
-    });
-
     coords
+        .iter()
+        .map(|p| {
+            let move_y = expand_row_idx.iter().filter(|i| **i < p.1).count();
+            let move_x = expand_col_idx.iter().filter(|i| **i < p.0).count();
+            (p.0 + move_x * multiplier, p.1 + move_y * multiplier)
+        })
+        .collect_vec()
 }
 
-fn solve(input: &str, part_two: bool) -> usize {
+fn solve(input: &str, multiplier: usize) -> usize {
     let coords = get_coords(input);
-    let expanded = expand_coords(coords, part_two);
+    let expanded = expand_coords(coords, multiplier);
 
     expanded
         .iter()
         .tuple_combinations::<(&Point, &Point)>()
         .fold(0, |acc, (a, b)| {
-            let diff = *a - *b;
+            let diff = a.0.abs_diff(b.0) + a.1.abs_diff(b.1);
             acc + diff
         })
 }
 
 fn main() {
-    solved!("Day 11 part 1: {}", solve(INPUT, false), 10033566);
-    solved!("Day 11 part 2: {}", solve(INPUT, true), 560822911938_usize);
+    solved!("Day 11 part 1: {}", solve(INPUT, 1), 10033566);
+    solved!("Day 11 part 2: {}", solve(INPUT, 999_999), 560822911938_usize);
 }
